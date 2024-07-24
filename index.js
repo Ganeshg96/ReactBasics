1abf95a943a7f14a48dc51024ac3747527c557c0
ganesh gone <ganeshgone2003@gmail.com>
2024-07-25 00:59:01 +0530

    created first website in React

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const page=(
+    <div>
+        <h1>My first Website i.e static in React</h1>
+        <h3>Reasons I love ❤️ React</h3>
+        <ol>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's actively maintained by skilled people</li>
+        </ol>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
