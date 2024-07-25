8f0b87cd5874ebc012709a4350356c1fdcfe01bd
ganesh gone <ganeshgone2003@gmail.com>
2024-07-25 10:12:11 +0530

    used parent-child components

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
+//Child Component
+function Header(){
+    return (
+        <header> 
+                <nav>
+                <img src="react-logo.png" className="small-image" alt="React Logo" />            
+                </nav>
+        </header>
+    );
+}
 
-const page=(
-    <div>
-        <h1>My first Website i.e static in React</h1>
-        <h3>Reasons I love ❤️ React</h3>
-        <ol>
-            <li>It's composable</li>
-            <li>It's declarative</li>
-            <li>It's actively maintained by skilled people</li>
-        </ol>
-    </div>
-)
+//Child Component
+function MainContent(){
+    return (
+        <div>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>was first released in 2013</li>
+                <li>was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+            </ul>
+        </div>
+    );
+}
 
-ReactDOM.render(page, document.getElementById("root"))
+//Child Component
+function Footer(){
+    return(
+        <footer>
+                <small>©️ 2024 ganesh development. all rights reserved</small> 
+        </footer>
+    );
+}
+
+//Parent Componenet
+function TemporaryPage () {
+    return (
+        <div>
+            <Header /> 
+            <MainContent />
+            <Footer />
+            
+        </div>
+    );
+
+}
+
+//ReactDOM.render(TemporaryPage() , document.getElementById("root"));
+ReactDOM.render(<TemporaryPage /> , document.getElementById("root"));
